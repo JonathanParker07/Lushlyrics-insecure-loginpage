96ef97d7f6030f309375d91fc92955d5290339d3
Jonathan Parker <jonathanparkerdonkor@gmail.com>
2024-11-15 15:04:39 +0000

    initial setup

---
+++ lush/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jonathan-parker/Documents/Projects/Lushlyrics-insecure-loginpage/lush/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
